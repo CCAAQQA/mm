c8471dc54d3aa7f0a3c90072fd375ae742342526
CCAAQQA <zydb70509@gmail.com>
2024-08-04 19:55:09 +0300

    Delete sosoMusic directory

--- sosoMusic/platforms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
